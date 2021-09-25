cef30bf296b94cd6fbd9d36eb82f7af18881cc4b
akanksha29-byte <akanksha2876@gmail.com>
2021-09-26 00:00:56 +0530

    second

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 const Card = ({ item }) => {
+  const date = new Date(+item.event_date);
+  const trim = (str) => {
+    return str.length > 10 ? str.substring(0, 10).trim() : str;
+  };
   return (
     <div className="card">
       <img src={item.thumbnail_image} alt={item.id} />
-      <h4 style={{ marginBottom: "5px" }}>Name: {item.event_name}</h4>
-      <h4 style={{ marginBottom: "5px" }}>
-        Date: {new Date(item.event_date).toDateString()}
-      </h4>
+      <h4 style={{ marginBottom: "5px" }}>Name: {trim(item.event_name)}</h4>
+      <h5 style={{ marginBottom: "5px", color: "gray" }}>
+        Date: {`${date.toLocaleString()}`}
+      </h5>
       <div className="card_details">
         <h5>Likes: {item.likes}</h5>
         <h5>Views: {item.views}</h5>
